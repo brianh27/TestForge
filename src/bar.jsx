07957507b75e4630090e248707551c7f03a74a78
brianh27 <brianh27@lakesideschool.org>
2024-09-12 13:19:47 -0700

    Update bar.jsx

--- src/bar.jsx
+++ src/bar.jsx
@@ -20,7 +20,7 @@ const Bar=({user})=>{
               <a onClick={() => navigate('/guides')} className="text-gray-300 hover:text-white mx-4 cursor-pointer">Guides</a>
               <a onClick={() => navigate('/puzzle')} className="text-gray-300 hover:text-white mx-4 cursor-pointer">Puzzles</a>
               <a onClick={() => navigate('/rush/main')} className="text-gray-300 hover:text-white mx-4 cursor-pointer">Rush</a>
-                <a href='https://discord.gg/UQbMpFyrPh' className="text-gray-300 hover:text-white mx-4 cursor-pointer">Join the Discord</a>
+                <a href='https://discord.gg/UQbMpFyrPh' target="_blank" className="text-gray-300 hover:text-white mx-4 cursor-pointer">Join the Discord</a>
                 <a 
                   onClick={() => user === null ? navigate('/login') : toggleDropdown()} 
                   className="text-gray-300 hover:text-white mx-4 cursor-pointer"
